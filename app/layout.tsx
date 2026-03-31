f8445f6400ca1d29f96fbc45edcb6ea31de447d1
shenzhou-web <shenzhouinvestment@gmail.com>
2026-03-31 16:14:18 +0400

    added X head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,10 +38,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* X conversion tracking base code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','rabuw');",
+          }}
+        />
+        {/* End X conversion tracking base code */}
+      </head>
       <body className={`font-sans antialiased`}>
-      <Web3Providers>
-        {children}
-      </Web3Providers>
+        <Web3Providers>{children}</Web3Providers>
       </body>
     </html>
   )
